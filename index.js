c66e6a8e13f01ce41f11d1e29696962750e8262a
Dor Danai <danaidor@gmail.com>
2022-06-01 20:12:20 +0300

    basic commands working

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-async function start() {
-  await getCocktail("mojito");
-}
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
 client.on("ready", async () => {
   console.log("ready ");
 });
