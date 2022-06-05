bfd89828c22460d5f577ac1e1377b87ac9764c1e
karnafun <danaidor@gmail.com>
2022-06-05 21:55:10 +0300

    refactoring

--- commands/cocktail.js
+++ commands/cocktail.js
@@ -1,6 +1,47 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { getCocktail } = require('../get-cocktails')
-const { MessageEmbed, MessageAttachment } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const axios = require("axios");
+function GetCocktailObject(data) {
+  
+  let cocktails = []
+
+  data.forEach(ct => {
+    let ingList = [];
+    for (i = 1; i <= 15; i++) {
+      let ing = ct["strIngredient" + i];
+      let amount = ct["strMeasure" + i];
+      if (ing&&amount ){
+        ingList.push(`${amount}of ${ing}`)
+      }else if (ing){
+        ingList.push(`add some ${ing}`)
+      }
+    }
+    cocktails.push({
+      name:ct.strDrink,
+      glass:ct.strGlass,
+      ingredients: ingList,
+      image: ct.strDrinkThumb,
+      instructions:ct.strInstructions,
+      print:function(){
+        let res = this.name +'\r\n'
+        res += `Served in: ${this.glass}`+'\r\n'
+        res+= "How to make:"+'\r\n'
+        res+= this.ingredients+'\r\n'
+        res+= "online instructions:"+'\r\n'
+        res+= this.instructions+'\r\n'
+        return res;
+      }
+    })
+  });
+
+  return cocktails;
+} 
+async function  getCocktail(name) {
+    let url = `http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
+    const response = await axios(url);
+    const data = response.data;
+    return GetCocktailObject(data.drinks);       
+  }
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,7 +50,7 @@ module.exports = {
         .addStringOption((option) =>
             option
                 .setName("cocktail_name")
-                .setDescription("The cocktail you want to make")
+                .setDescription("The name of the cocktail")
                 .setRequired(true)
         ),
     async execute(interaction) {
@@ -18,10 +59,10 @@ module.exports = {
         console.log("The name is :", name)
         let res = await getCocktail(name)
         const embed = new MessageEmbed()
-        .setImage(res[0].image)
-        .setTitle(res[0].name)
-        .setDescription(res[0].print())
-        .setColor('#0099ff')                
-        await interaction.reply({ embeds: [embed]})        
+            .setImage(res[0].image)
+            .setTitle(res[0].name)
+            .setDescription(res[0].print())
+            .setColor('#0099ff')
+        await interaction.reply({ embeds: [embed] })
     },
 };
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const fs = require("node:fs");
-const path = require("node:path");
 const { registerAllFunctions } = require('./deploy-commands')
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
@@ -12,24 +10,13 @@ client.commands = new Discord.Collection();
 client.on("ready", async () => {
   await registerAllFunctions(client)
   console.log("ready ");
-  
-
 });
 
 client.on("interactionCreate", async (interaction) => {
   console.log("interaction received: ", interaction.commandName)
-  if (!interaction.isCommand()) {
-    console.log("invalid interaction: " + interaction.commandName)
-    return;
-  };
-  console.log("all commands:",client.commands)
-  
-  let test = client.commands.get('ping')
-  console.log("ping function:",test)
   const command = client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.log("not a command ");
+  if (!interaction.isCommand() || !command) {
+    console.log("invalid interaction: " + interaction.commandName)
     return;
   };
 
@@ -43,6 +30,5 @@ client.on("interactionCreate", async (interaction) => {
     });
   }
 });
-
 
 client.login(process.env.TOKEN);
